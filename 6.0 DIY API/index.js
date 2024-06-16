777fcd6d14bbb017b8e0a6c359eb57a394afde3f
phphuongbinh <mrsplol102@gmail.com>
2024-06-16 23:41:15 +0700

    put route

--- 6.0 DIY API/index.js
+++ 6.0 DIY API/index.js
@@ -32,7 +32,28 @@ app.get("/filter", (req, res) => {
 
 //4. POST a new joke
 
+app.post("/jokes", (req, res) => {
+  const newJoke = {
+    id: jokes.length + 1,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
+  jokes.push(newJoke);
+  res.json(newJoke);
+});
+
 //5. PUT a joke
+app.put("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const replacementJoke = {
+    id: id,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  jokes[searchIndex] = replacementJoke;
+  res.json(replacementJoke);
+});
 
 //6. PATCH a joke
 
